4e051184fbafb99732271e41818ed36b984e14af
Joakim Sigvald <joakim.sigvald@medrave.com>
2026-07-12 12:24:48 +0200

    support multiple .net versions

--- Core.Test/Assert/WhenList.cs
+++ Core.Test/Assert/WhenList.cs
@@ -19,7 +19,7 @@ public class WhenList : Spec<List<string>>
     {
         var values = new[] { "a", "b" };
         var list1 = values.ToList();
-        var list2 = values.Reverse();
+        var list2 = values.AsEnumerable().Reverse();
         list1.Is().Like(list2);
         Specification.Is("List1 is like list2");
     }
@@ -29,7 +29,7 @@ public class WhenList : Spec<List<string>>
     {
         var values = new[] { "a", "b" };
         var list1 = values.ToList();
-        var list2 = values.Reverse();
+        var list2 = values.AsEnumerable().Reverse();
         list1.Is().EquivalentTo(list2);
         Specification.Is("List1 is equivalent to list2");
     }
